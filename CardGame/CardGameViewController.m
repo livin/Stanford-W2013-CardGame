b5eb11f4e5d8cd4c8e834e3a84caa3ea46b103e3
Vladimir Livin <vladimir@livesystems.ru>
2013-03-24 23:16:01 +0400

    Added card to the main view which can be flipped now. Flips count is shown in the bottom of the screen.

--- CardGame/CardGameViewController.m
+++ CardGame/CardGameViewController.m
@@ -10,20 +10,24 @@
 
 @interface CardGameViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+@property (nonatomic) int flipCount;
+
 @end
 
 @implementation CardGameViewController
 
-- (void)viewDidLoad
+
+- (void) setFlipCount:(int)flipCount
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	_flipCount = flipCount;
+	self.flipsLabel.text = [NSString stringWithFormat: @"Flips: %d", self.flipCount];
+	NSLog(@"flips updated to %d", self.flipCount);
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)flipCard:(UIButton *)sender {
+	sender.selected = !sender.isSelected;
+	self.flipCount++;
 }
 
 @end
